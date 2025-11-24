aa7dbbecc0235c58bcb4d890cc4e71371e928def
janbsc4 <janbscholl@gmail.com>
2025-11-24 12:02:57 +0100

    Added web app offline behaviour

---
+++ offline.md
@@ -0,0 +1,6 @@
+---
+layout: default
+title: "You are offline"
+---
+# You are offline
+And this page hasn't been saved yet. Woops.
---
+++ sw.js
@@ -0,0 +1,64 @@
+---
+layout: null
+---
+
+const CACHE_NAME = 'janbalanya{{ site.time | date: "%s" }}';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		fetch('/cache-list.json')
+		.then(response => response.json())
+		.then(files => {
+			console.log('Service Worker: Caching ' + files.length + ' files from manifest...');
+			return caches.open(CACHE_NAME).then(cache => {
+				return cache.addAll(files);
+			});
+		})
+		.catch(error => {
+			console.error('Failed to cache files:', error)
+		})
+		);
+});
+
+self.addEventListener('fetch', event => {
+	if (event.request.method !== 'GET') return;
+
+	event.respondWith(
+		fetch(event.request)
+		.then(response => {
+			if (!response || response.status !== 200 || response.type !== 'basic') {
+				return response;
+			}
+			const responseToCache = response.clone();
+			caches.open(CACHE_NAME).then(cache => {
+				cache.put(event.request, responseToCache);
+			});
+			return response;
+		})
+		.catch(() => {
+			return caches.match(event.request).then(cachedResponse => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
+				if (event.request.headers.get('accept').includes('text/html')) {
+					return caches.match(OFFLINE_URL);
+				}
+			});
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(cache => {
+					if (cache !== CACHE_NAME) {
+						return caches.delete(cache);
+					}
+				})
+			);
+		})
+	);
+});
